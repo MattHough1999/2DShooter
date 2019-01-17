cc67b7d121e1de507d4329833e09a8f0d455794a
MattHough1999 <matthough.uk@gmail.com>
2019-01-17 03:07:09 +0000

    attempt to add sound
    attempted to add sound effects, dont seem to be playing but might in build

---
+++ Assets/Scripts/PlayEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEffect : MonoBehaviour {
+
+    public AudioClip Effect;
+    public AudioSource Source;
+	// Use this for initialization
+	void Start () {
+        Source.clip = Effect;
+        Source.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
