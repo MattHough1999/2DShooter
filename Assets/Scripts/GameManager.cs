68bcc4273461e5437c53f9c2a777067c7b38c821
MattHough1999 <matthough.uk@gmail.com>
2019-01-17 02:32:59 +0000

    added auto level
    changes level when all objectives are destroyed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,4 +19,33 @@ public class GameManager : MonoBehaviour
     {
         Application.Quit();
     }
+    public void CheckSpawners()
+    {
+        if (SceneManager.GetActiveScene().name != "Menu" && SceneManager.GetActiveScene().name != "Game Over")
+        {
+            if (GameObject.Find("Spawner") == null)
+            {
+                SceneManager.LoadScene(nextScene());
+            }
+        }
+    }
+    public string nextScene()
+    {
+        if (SceneManager.GetActiveScene().name == "Tutorial")
+        {
+            return "LevelOne";
+        }
+        else if (SceneManager.GetActiveScene().name == "LevelOne")
+        {
+            return "LevelTwo";
+        }
+        else if (SceneManager.GetActiveScene().name == "LevelTwo")
+        {
+            return "Game Over";
+        }
+        else
+        {
+            return "menu";
+        }
+    }
 }
