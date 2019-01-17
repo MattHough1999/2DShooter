0767676996655dd4eda4523bd8763c3442fdc202
MattHough1999 <matthough.uk@gmail.com>
2019-01-17 00:27:54 +0000

    Added Menu
    Added menu functionality

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
